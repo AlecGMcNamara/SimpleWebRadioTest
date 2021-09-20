2a49273b70535d9ea832fbb8ad53027b1055eb14
Alec McNamara <alecgmcnamara@gmail.com>
2021-09-20 15:29:35 +0100

    Greatet Hits Radio Web Radio

--- src/main.cpp
+++ src/main.cpp
@@ -14,58 +14,33 @@ String password = "8NHetSWQAJ75";
 bool beat = false;
 void setup() {
   Serial.begin(115200);
- // WiFi.disconnect();
- // WiFi.mode(WIFI_STA);
- // Serial.println("Starting WiFi");
- // WiFi.begin(ssid.c_str(), password.c_str());
- // while (WiFi.status() != WL_CONNECTED)
- // delay(1500);
- // Serial.println("WiFi Connected!");
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  Serial.println("Starting WiFi");
+  WiFi.begin(ssid.c_str(), password.c_str());
+  while (WiFi.status() != WL_CONNECTED)
+  delay(1500);
+  Serial.println("WiFi Connected!");
 
 // Launch SPIFFS file system  
- if(!SPIFFS.begin()){ 
-    Serial.println("An Error has occurred while mounting SPIFFS");  
-  }
+// if(!SPIFFS.begin()){ 
+//    Serial.println("An Error has occurred while mounting SPIFFS");  
+//  }
   
   audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-  audio.setVolume(10);
+  audio.setVolume(5);
 
   //audio.connecttoFS(SPIFFS,"/test.mp3" );
   //audio.connecttoFS(SPIFFS,"/kick.wav" );
-  audio.connecttoFS(SPIFFS,"/kick.wav" );
+  //audio.connecttoFS(SPIFFS,"/kick.wav" );
 
-//  audio.connecttohost("vis.media-ice.musicradio.com/CapitalMP3");
-//  audio.connecttohost("http://0n-80s.radionetz.de:8000/0n-70s.mp3");
-//  audio.connecttohost("http://mediaserv30.live-streams.nl:8000/stream");
-//  audio.connecttohost("http://www.surfmusic.de/m3u/100-5-das-hitradio,4529.m3u");
-//  audio.connecttohost("http://stream.1a-webradio.de/deutsch/mp3-128/vtuner-1a");
-//  audio.connecttohost("http://mp3.ffh.de/radioffh/hqlivestream.aac");
+// Greatest hits radio Stafford and cheshire
+audio.connecttohost("www.radiofeeds.co.uk/bauerflash.pls?station=net2stoke.mp3.m3u");
 
 }
 void loop()
 {
     audio.loop();
-    static long int LastTime = 0;
-    long int CurrTime = millis();
-
-    if(CurrTime-LastTime > 1000)
-    {
-        audio.stopSong();
-        if(beat)
-        {
-            audio.setVolume(21);
-            audio.connecttoFS(SPIFFS,"/kick.wav" );
-            beat = false;
-        }
-        else
-        {
-            audio.setVolume(8);
-            audio.connecttoFS(SPIFFS,"/snare.wav" );
-            beat = true;
-        }
-        LastTime = CurrTime;
-    }
-
 }
 // optional
 void audio_info(const char *info){
@@ -75,16 +50,7 @@ void audio_id3data(const char *info){  //id3 metadata
     Serial.print("id3data     ");Serial.println(info);
 }
 void audio_eof_mp3(const char *info){  //end of file
-    //Serial.print("eof_mp3     ");Serial.println(info);
-    //delay(10);
-    //if (beat){
-    //    audio.connecttoFS(SPIFFS,"/kick.wav" );
-    //    beat = false;
-     //}
-     //else{
-     //    audio.connecttoFS(SPIFFS,"/snare.wav" );
-     //   beat = true;
-     //}
+    Serial.print("eof_mp3     ");Serial.println(info);
     
 }
 void audio_showstation(const char *info){
